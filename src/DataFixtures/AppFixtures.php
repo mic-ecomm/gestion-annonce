d3e503b599f6416ec1531442d9545436d368b463
Jean Michel <jmslfoo@gmail.com>
2023-08-10 10:24:58 +0300

    teste d'avatar aleatoire pour nos fausses donnéees

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,15 +18,25 @@ class AppFixtures extends Fixture
         //Nous gérons les utilisateurs
 
         $users = [];
+        $genres = ['male', 'female'];
 
         for ($i = 0; $i <= 10; $i++) {
             $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $picture =  'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1, 99) . 'jpg';
+
+            $picture .= ($genre == "male" ? 'men/' : 'women/') . $pictureId;
+
             $user->setFirstName($faker->firstname)
                 ->setLastName($faker->lastname)
                 ->setEmail($faker->email)
                 ->setIntroduction($faker->sentence())
                 ->setDescription('<p>' . implode('</p><p>', $faker->paragraphs(3)) . '</p>')
-                ->setHash('password');
+                ->setHash('password')
+                ->setPicture($picture);
 
             $manager->persist($user);
             $users[] = $user;
