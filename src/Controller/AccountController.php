111a6ef2e2d1441e1b2d4cff28b09bbf3d7db08a
Jean Michel <jmslfoo@gmail.com>
2023-08-11 10:46:11 +0300

    réperage des erreurs sur l'authentification

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Psr7\Response as Psr7Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
+     * Permet d'afficher et de gérer les formulaires de connexion
+     * 
      * @Route("/login", name="account_login")
+     * 
+     * @return Response
+     * 
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $utils): Response
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render(
+            'account/login.html.twig',
+            [
+                'hasError' => $error !== null,
+                'username' => $username
+            ]
+        );
+    }
+
+    /**
+     * Permet de se déconnecter
+     * 
+     * @Route("/logout", name="account_logout")
+     * 
+     * @return void
+     * 
+     */
+    public function logout()
+    {
+        //Ne me rien ici
     }
 }
